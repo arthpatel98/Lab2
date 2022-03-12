3c1b0d6653141b1b3ba0299e5fa567181c20a211
Arthkumar Patel <arthp7044@gmail.com>
2022-03-11 19:56:21 -0800

    Final Code

---
+++ app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.zybooks.pizzaparty
+
+
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.ceil
+
+/**
+ * MainActivity class contains the Kotlin code that interacts with the UI.
+ * MainActivity inherits the AppCompatActivity class which is the superclass for all activities and provides the
+ latest Android functionality
+ */
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var numAttendEditText: EditText
+    private lateinit var numPizzasTextView: TextView
+    private lateinit var howHungryRadioGroup: RadioGroup
+    /**
+     * The Activity method onCreate() is the first method called when the activity starts, it loads the activity's XML
+     layout and performs other initialization logic
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        numAttendEditText = findViewById(R.id.num_attend_edit_text)
+        numPizzasTextView = findViewById(R.id.num_pizzas_text_view)
+        howHungryRadioGroup = findViewById(R.id.hungry_radio_group)
+    }
+    /**
+     *  Function to calculate no. of pizza and take necessary inputs.
+     */
+    fun calculateClick(view: View) {
+
+        // Get the text that was typed into the EditText
+        val numAttendStr = numAttendEditText.text.toString()
+
+        // Convert the text into an integer
+        val numAttend = numAttendStr.toIntOrNull() ?: 0
+
+        // Get hunger level selection
+        val hungerLevel = when (howHungryRadioGroup.getCheckedRadioButtonId()) {
+            R.id.light_radio_button -> PizzaCalculator.HungerLevel.LIGHT
+            R.id.medium_radio_button -> PizzaCalculator.HungerLevel.MEDIUM
+            else -> PizzaCalculator.HungerLevel.RAVENOUS
+        }
+
+        // Get the number of pizzas needed
+        val calc = PizzaCalculator(numAttend, hungerLevel)
+        val totalPizzas = calc.totalPizzas
+
+        // Place totalPizzas into the string resource and display
+        val totalText = getString(R.string.total_pizzas, totalPizzas)
+        numPizzasTextView.setText(totalText)
+    }
+}
